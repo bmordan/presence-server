095d39a67a6df6dd530968928a1761b180205abe
Bernard Mordan <bernardmordan@gmail.com>
2014-08-26 10:22:08 +0100

    OK wired together and serving basic pages

--- client/main.js
+++ client/main.js
@@ -11,4 +11,15 @@ Meteor.startup(function(){
       }
     })
   })//end of Router.map
+  
+  // Add route to body
+  var routes = Router.routes.map(function(r){return r.name}).join(' ')
+  Deps.autorun(function(){
+    var $body = $('body')
+    $body.removeClass(routes)
+    var currentRoute = Router.current()
+    if (currentRoute && currentRoute.route && currentRoute.route.name){
+      $body.addClass(currentRoute.route.name)
+    }
+  })
 })
