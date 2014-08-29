dda8f0d79da1a18eff93cb37b995bb32328b5a6f
Bernard Mordan <bernardmordan@gmail.com>
2014-08-29 10:15:47 +0100

    tidy logic around updating status, and query only 'online' to display inmates on home page

--- both/visitors.js
+++ both/visitors.js
@@ -2,9 +2,11 @@ Visitors = new Meteor.Collection('visitors')
 
 Visitors.manager = function(mac,ts,status){
   Visitors.upsert(mac,{'_id':mac,'mac': mac,'ts':ts,'status':status})
+
   var inmate = Inmates.findOne({mac: mac})
   if(inmate.length === 1) {
-    if(status === 'undefined') status = 'Doing Time'
-    Inmates.update(inmate._id,{ts:ts, status: status})
+    var query = {ts:ts}
+    if(status) query.status = status
+    Inmates.update(inmate._id, query)
   }
 }
