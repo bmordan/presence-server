e8cffabd18df88d3222e52a57ea88e122a05d602
Bernard Mordan <bernardmordan@gmail.com>
2014-08-29 14:51:39 +0100

    photo uploading added

--- client/addphoto/addphoto.js
+++ client/addphoto/addphoto.js
@@ -1,15 +1,44 @@
 Template.upload.events({
-  'click .upload' : function(evt,tpl){
+  'change #face, change #profile' : function(evt,tpl){
     evt.preventDefault()
-    
-    var _id = tpl.find('#id').value
-    var photo = tpl.find('#file').files[0].name
-    console.log(photo)
+    var type = evt.target.id
+    photoReader(type, evt)
+  },
+  'click .preview' : function(evt, tpl){
+    evt.preventDefault()
+    var type = evt.target.dataset.for
+    console.log(evt)
+    $('#'+type).trigger('click')
+  },
+  'submit' : function(evt, tpl){
+    evt.preventDefault()
+    Inmates.update(this.id,
+      {$set: {mugshots: {
+        face: Session.get('previewface'),
+        profile: Session.get('previewprofile')
+      }
+    }})
+    Router.go('jail')
+  }
+  
+})
 
-    // var reader = new FileReader()
-    // reader.onload = function(evt) {
-    //   Inmate.update(_id, {$set: {mugshots: {face: evt.target.result}} })
-    // }
-    // reader.readAsDataURL(photo)
+Template.upload.helpers({
+  'previewface' : function(){
+    return Session.get('previewface')
+  },
+  'previewprofile' : function(){
+    return Session.get('previewprofile')
   }
 })
+
+function photoReader(type, evt){
+  var files = evt.target.files
+  var file = files[0]
+  if(!file.type.match('image.*')) return console.error('wrong file format %s', file.name)
+  var reader = new FileReader()
+  reader.onload = function(evt){
+    Session.set('preview'+type,evt.target.result)
+  }
+  reader.readAsDataURL(file)
+}
