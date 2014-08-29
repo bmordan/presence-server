0173130f235e06cb48fb8a1a12b7ee87a0892657
Bernard Mordan <bernardmordan@gmail.com>
2014-08-29 09:43:48 +0100

    syslog also updates time

--- both/visitors.js
+++ both/visitors.js
@@ -5,6 +5,6 @@ Visitors.manager = function(mac,ts,status){
   var inmate = Inmates.findOne({mac: mac})
   if(inmate.length === 1) {
     if(status === 'undefined') status = 'Doing Time'
-    Inmates.update(inmate._id,{status: status})
+    Inmates.update(inmate._id,{ts:ts, status: status})
   }
 }
