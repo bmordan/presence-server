bff4a9bfbe429db0a9525639dfa09eb2d313bac7
Liam Wooding <liamwooding@gmail.com>
2014-09-01 14:05:19 +0100

    /add page shows visitors' status, added @msdosh to settings.json

--- both/visitors.js
+++ both/visitors.js
@@ -2,14 +2,19 @@ Visitors = new Meteor.Collection('visitors')
 
 Visitors.manager = function(mac,ts,status){
   // update latest message for this mac
-  Visitors.upsert(mac,{'_id':mac,'mac': mac,'ts':ts,'status':status})
+  console.log(status)
+  var visitor = {'mac': mac, 'ts':ts}
+  if (status === 'offline' || status === 'online') visitor.status = status
+
+  //console.log(visitor)
+  Visitors.upsert(mac, {$set: visitor})
   
   // Do we know this mac?
   var inmate = Inmates.findOne({mac: mac})  
   if (!inmate) return
 
-  // Yes! we do, let's updat them...
+  // Yes! we do, let's update them...
   var query = {ts:ts}
-  if(status) query.status = status
+  if (status) query.status = status
   Inmates.update(inmate._id, {$set: query})
 }
